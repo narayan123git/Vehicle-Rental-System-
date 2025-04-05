91a34de49c3a81bdd403474375987078e3cf1149
Narayan Paul <158485405+narayan123git@users.noreply.github.com>
2025-04-05 21:52:41 +0530

    Uploaded files #2

--- Vehicle_Rental_System/SQL_Scripts/queries.sql
+++ Vehicle_Rental_System/SQL_Scripts/queries.sql
@@ -14,25 +14,26 @@ HAVING COUNT(*) > 3;
 UPDATE Vehicles
 SET AvailabilityStatus = 'Not Available'
 WHERE VehicleID = 2;
+SELECT * FROM Vehicles;
 
 SELECT * FROM Rentals
 WHERE RentDate >= CURDATE() - INTERVAL 30 DAY;
 
 DELETE FROM Rentals
 WHERE RentDate < CURDATE() - INTERVAL 1 YEAR;
+SELECT * FROM Rentals;
 
 SELECT VehicleID, COUNT(*) AS TimesRented
 FROM Rentals
 GROUP BY VehicleID
 ORDER BY TimesRented DESC;
 
-SELECT CustomerID, DATEDIFF(ReturnDate, RentDate) AS RentalDuration
+SELECT CustomerID, DATEDIFF(ReturnDate, RentDate) AS TripLength
 FROM Rentals
-ORDER BY RentalDuration DESC;
+ORDER BY TripLength DESC;
 
 SELECT v.VehicleID, v.Model, SUM(r.TotalCost) AS TotalEarnings
 FROM Vehicles v
 JOIN Rentals r ON v.VehicleID = r.VehicleID
 GROUP BY v.VehicleID, v.Model
 ORDER BY TotalEarnings DESC;
-
