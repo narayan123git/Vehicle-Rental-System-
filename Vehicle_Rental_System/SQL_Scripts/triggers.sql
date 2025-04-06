d0b0f46d101cd436cdc2e7c68173e499b12f0ac8
Narayan Paul <158485405+narayan123git@users.noreply.github.com>
2025-04-06 19:51:49 +0530

    Update triggers.sql

--- Vehicle_Rental_System/SQL_Scripts/triggers.sql
+++ Vehicle_Rental_System/SQL_Scripts/triggers.sql
@@ -1,3 +1,4 @@
+-- Trigger for setting the availability status after renting a vehicle
 DELIMITER //
 CREATE TRIGGER UpdateAvailabilityAfterRental
 AFTER INSERT ON Rentals
@@ -8,7 +9,7 @@ BEGIN
     WHERE VehicleID = NEW.VehicleID;
 END;
 //
-
+-- Trigger for setting the availability status after returning(updating return date in Vehicles table) a vehicle
 CREATE TRIGGER UpdateAvailabilityOnReturn
 AFTER UPDATE ON Rentals
 FOR EACH ROW
